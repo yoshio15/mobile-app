4a7b98095d4e89fda1ee8cdfe28ab2fedec245f4
YoshioKondo <yoshio15310@gmail.com>
2020-11-05 22:43:04 +0900

    create App Header

--- App.tsx
+++ App.tsx
@@ -1,27 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Button, Icon, Header } from 'react-native-elements';
+import { StyleSheet, View } from 'react-native';
+import { Header } from 'react-native-elements';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Header></Header>
-
-      <Text>
-        Hello World!!
-      </Text><Button
-          large
-          backgroundColor='#009688'
-          icon={{name:'android'}}
-          title='Hello!'
-        />
-      <StatusBar style="auto" />
+    <View style={styles.body}>
+      <Header
+        leftComponent={{ icon: 'menu', color: '#fff' }}
+        centerComponent={{ text: 'フットサルナビ', style: { color: '#fff' } }}
+      ></Header>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-  },
+  body: {},
 });
