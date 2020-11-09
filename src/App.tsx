aa093c7c48211364b09d09d5de68b63bce0daa7f
YoshioKondo <yoshio15310@gmail.com>
2020-11-09 22:14:45 +0900

    add plus icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,27 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import AppHeader from './components/AppHeader';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 export default function App() {
   return (
     <View style={styles.body}>
       <AppHeader />
+      <View style={styles.plusIcon}>
+        <Icon
+          name="plus-circle"
+          size={60}
+          color='#00008b'
+        />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   body: {},
+  plusIcon: {
+    margin: 15,
+    alignItems: 'flex-end'
+  }
 });
