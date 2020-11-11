b942bb308821611e72365f1975ff5400d485d7a2
YoshioKondo <yoshio15310@gmail.com>
2020-11-11 22:03:50 +0900

    add new memo

---
+++ src/screens/NewMemo.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View } from 'react-native';
+import AppHeader from '../components/AppHeader';
+
+const NewMemo = () => {
+  return (
+    <View>
+      <AppHeader />
+    </View>
+  );
+}
+
+export default NewMemo;
