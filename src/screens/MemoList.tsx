5d6e19e6c8007467553c84e8bae94ec4ea8eae85
YoshioKondo <yoshio15310@gmail.com>
2020-11-11 22:18:03 +0900

    add Page Transition

--- src/screens/MemoList.tsx
+++ src/screens/MemoList.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { StackScreenProps } from '@react-navigation/stack';
 import AppHeader from '../components/AppHeader';
 import PlusIcon from '../components/PlusIcon';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 type RootStackParamList = {
   MemoList: undefined;
@@ -11,11 +12,26 @@ type RootStackParamList = {
 
 type Props = StackScreenProps<RootStackParamList, 'MemoList'>;
 
+
+const styles = StyleSheet.create({
+  plusIcon: {
+    margin: 15,
+    alignItems: 'flex-end'
+  }
+});
+
 const MemoList = ({ route, navigation }: Props) => {
   return (
     <View>
       <AppHeader />
-      <PlusIcon />
+      <View style={styles.plusIcon}>
+        <Icon
+          name="plus-circle"
+          size={60}
+          color='#00008b'
+          onPress={() => navigation.navigate('NewMemo')}
+        />
+      </View>
     </View>
   );
 }
