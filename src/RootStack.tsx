4269fb92ec2db4da6386270636ffca25e35df78a
YoshioKondo <yoshio15310@gmail.com>
2020-11-11 22:04:07 +0900

    add rootstackparamlist

--- src/RootStack.tsx
+++ src/RootStack.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 import MemoList from './screens/MemoList'
+import NewMemo from './screens/NewMemo'
 
-const Stack = createStackNavigator();
+type RootStackParamList = {
+  MemoList: undefined;
+  NewMemo: undefined;
+}
+
+const Stack = createStackNavigator<RootStackParamList>();
 
 const RootStack = () => {
   return (
     <Stack.Navigator>
       <Stack.Screen name='MemoList' component={MemoList}/>
+      <Stack.Screen name='NewMemo' component={NewMemo}/>
     </Stack.Navigator>
   );
 }
