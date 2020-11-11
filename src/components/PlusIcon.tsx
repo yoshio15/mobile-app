eea50f017afbe761df27eafe4e7cb4a860bc6683
YoshioKondo <yoshio15310@gmail.com>
2020-11-11 23:07:22 +0900

    use PlusIcon component

--- src/components/PlusIcon.tsx
+++ src/components/PlusIcon.tsx
@@ -2,13 +2,18 @@ import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
-const PlusIcon = () => {
+type Props = {
+  navigation: any
+}
+
+const PlusIcon = (props: Props) => {
   return (
     <View style={styles.plusIcon}>
       <Icon
         name="plus-circle"
         size={60}
         color='#00008b'
+        onPress={() => props.navigation.navigate('NewMemo')}
       />
     </View>
   );
--- src/screens/MemoList.tsx
+++ src/screens/MemoList.tsx
@@ -1,31 +1,14 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { View } from 'react-native';
 import AppHeader from '../components/AppHeader';
 import PlusIcon from '../components/PlusIcon';
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
-import { RootStackParamList, MemoListProps } from '../types'
+import { MemoListProps } from '../types'
 
-
-
-const styles = StyleSheet.create({
-  plusIcon: {
-    margin: 15,
-    alignItems: 'flex-end'
-  }
-});
-
-const MemoList = ({ route, navigation }: MemoListProps) => {
+const MemoList: React.FC<MemoListProps> = ({ route, navigation }: MemoListProps) => {
   return (
     <View>
       <AppHeader />
-      <View style={styles.plusIcon}>
-        <Icon
-          name="plus-circle"
-          size={60}
-          color='#00008b'
-          onPress={() => navigation.navigate('NewMemo')}
-        />
-      </View>
+      <PlusIcon navigation={navigation}></PlusIcon>
     </View>
   );
 }
