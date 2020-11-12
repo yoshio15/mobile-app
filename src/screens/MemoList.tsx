f1808641a8fe12e424449644eb9736ac491162f7
YoshioKondo <yoshio15310@gmail.com>
2020-11-12 23:16:56 +0900

    remove header

--- src/screens/MemoList.tsx
+++ src/screens/MemoList.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import { View } from 'react-native';
-import AppHeader from '../components/AppHeader';
 import PlusIcon from '../components/PlusIcon';
 import { MemoListProps } from '../types'
 
 const MemoList: React.FC<MemoListProps> = ({ route, navigation }: MemoListProps) => {
   return (
     <View>
-      <AppHeader />
       <PlusIcon navigation={navigation}></PlusIcon>
     </View>
   );
