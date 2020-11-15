3d3f3f653bd17fc47f824a94dcfab7bebae5d5a8
YoshioKondo <yoshio15310@gmail.com>
2020-11-15 22:51:08 +0900

    add create db sql

--- src/screens/MemoList.tsx
+++ src/screens/MemoList.tsx
@@ -4,16 +4,33 @@ import { Card } from 'react-native-elements';
 import * as SQLite from 'expo-sqlite';
 
 import PlusIcon from '../components/PlusIcon';
-import { MemoListProps } from '../types'
+import { MemoListProps } from '../types';
 
 // constants
 const DB_NAME = 'memo-app';
+const CREATE_DB_SQL = 'create table if not exists memos (memo_id integer primary key not null, content text);'
+const SELECT_ALL_SQL = 'select * from memos';
 
 const MemoList: React.FC<MemoListProps> = ({ _route, navigation }: MemoListProps) => {
 
   useEffect(() => {
     const db = SQLite.openDatabase(DB_NAME);
     console.log(db);
+    db.transaction(
+      // callback
+      (tx) => {
+        tx.executeSql(
+          CREATE_DB_SQL,
+          null,
+          () => { console.log('success') },
+          () => { console.log('fail') }
+        )
+      },
+      // error
+      () => { console.log('fail') },
+      // succuess
+      () => { console.log('success') }
+    )
   });
 
   return (
