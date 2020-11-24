3c4698e8a5f187edaa1702aaba649edcc9e05d5b
YoshioKondo <yoshio15310@gmail.com>
2020-11-24 22:03:18 +0900

    add insert method

--- src/screens/NewMemo.tsx
+++ src/screens/NewMemo.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { StyleSheet, View, TextInput } from 'react-native';
 import { Button } from 'react-native-elements';
+import { WebSQLDatabase } from 'expo-sqlite/src/SQLite.types';
+
+const DB_NAME = 'memo-app';
+const INSERT_DATA_SQL = `insert into ${DB_NAME} values (?, ?);`
 
 const NewMemo: React.FC = () => {
   return (
@@ -23,10 +27,6 @@ const NewMemo: React.FC = () => {
   );
 }
 
-const addMemo = () => {
-  console.log('addMemo')
-}
-
 const styles = StyleSheet.create({
   textAreaContainer: {
     borderColor: 'gray',
@@ -38,5 +38,29 @@ const styles = StyleSheet.create({
     justifyContent: "flex-start"
   }
 });
+
+const addMemo = () => {
+  console.log('addMemo')
+}
+
+// Database
+let db: WebSQLDatabase;
+
+// Insert Data
+const insert = (id:number, content: string) => {
+  db.transaction(
+    (tx) => {
+      tx.executeSql(
+        INSERT_DATA_SQL,
+        [id, content],
+        () => { console.log('insert data success.') },
+        () => {
+          console.log('insert data failed.')
+          return false;
+        }
+      )
+    },
+  )
+}
 
 export default NewMemo;
